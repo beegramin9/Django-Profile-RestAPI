f22c2160a734d597c628bc21da766d8a37b0ef33
wontae <beegramin9@gmail.com>
2021-02-20 17:59:19 +0900

    as_view(): Postman 역할

--- app/profiles_api/urls.py
+++ app/profiles_api/urls.py
@@ -4,5 +4,7 @@ from profiles_api import views
 # controller와 service를 연결
 urlpatterns = [
     # view 클래스를 해당 url에 render
+    # 이게 NestJS의 controller - service이랑 다른 점
+    # as_view()로 html로 render해서 Postman 역할을 한다
     path('hello-view/', views.HelloAPIView.as_view()),
 ]
