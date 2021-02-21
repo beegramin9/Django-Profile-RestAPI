00f7d0aac1b936b9e75c490d821d605ad79e9dd2
wontae <beegramin9@gmail.com>
2021-02-21 15:35:09 +0900

    profile viewset 등록

--- app/profiles_api/urls.py
+++ app/profiles_api/urls.py
@@ -9,6 +9,12 @@ router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 """ router가 연결이 되면 ViewSet과 관련된 url의 리스트를 만든다
 그 리스트기 router.urls고 밑에서 include해주는 것 """
 
+""" viewset에 queryset이 정해져있으면 그 query가 타겟으로 한 모델을 보고
+쟝고 drf가 모델을 알아채기 때문에 base_name 줄 필요 없다.
+즉 queryset이 없거나 viewset을 override하고싶을떄만 base_name을 준다 """
+router.register('profile', views.UserProfileViewSet)
+
+
 # controller와 service를 연결
 urlpatterns = [
     # view 클래스를 해당 url에 render
